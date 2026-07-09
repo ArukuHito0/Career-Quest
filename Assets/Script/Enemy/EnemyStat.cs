d788b9a292335e6070d11c131acd296a2afc6196
a9r1311 <a9r1311@outlook.jp>
2026-07-09 10:54:51 +0900

    敵のステータス作成

---
+++ Assets/Script/Enemy/EnemyStat.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace CareerQuest.Enemy
+{
+    //  “G‚Ì”\—Í’l
+    [System.Serializable]
+    public sealed class EnemyStat
+    {
+        [Header("‘Ì—Í")]
+        public int HP = 100;
+        [Header("ˆÚ“®‘¬“x")]
+        public float MoveSpd = 5;
+    }
+}
